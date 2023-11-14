b3f9e047fe8662292cffc7a0f87d11f0a8ce8a55
David Brennan <thedavil@gmail.com>
2023-11-14 23:27:11 +0000

    Updated VR controller script
    - Added a way to disable multiple components on the (non-self) controller
    - Moved from _avatar.IsMe to newer IsOwner (IsMe deprecated)
    
    also some minor changes to Player prefab etc.

--- Assets/DavScripts/PlayerVRController.cs
+++ Assets/DavScripts/PlayerVRController.cs
@@ -3,7 +3,12 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using Avatar = Alteruna.Avatar;
-using TrackedPoseDriver = UnityEngine.SpatialTracking.TrackedPoseDriver;
+
+using UnityEngine.XR.Interaction.Toolkit;
+using UnityEngine.XR.Interaction.Toolkit.Inputs;
+using UnityEngine.XR.Interaction.Toolkit.Samples.StarterAssets;
+using UnityEngine.XR.Interaction.Toolkit.UI;
+using Unity.VRTemplate;
 
 public class PlayerVRController : MonoBehaviour
 {
@@ -30,18 +35,44 @@ public class PlayerVRController : MonoBehaviour
         }
         else
         {
-            // leftController.enabled = false;
-            // rightController.enabled = false;
+            //Disable control components on the controller (other person, not self)
+            DisableComponentsOnController(leftController);
+            DisableComponentsOnController(rightController);
             camera.gameObject.SetActive(false);
         }
     }
 
     private void Update()
     {
-        if (!_avatar.IsMe)
+        if (!_avatar.IsOwner)
             return;
 
         head.localPosition = camera.transform.localPosition;
         head.rotation = camera.transform.rotation;
+    }
+
+
+
+
+
+     private void DisableComponentsOnController(GameObject controller)
+    {
+        // Replace these types with the actual component types you want to disable
+        var actionBasedControllerManager = controller.GetComponent<ActionBasedControllerManager>();
+        var xrController = controller.GetComponent<XRController>(); // Assuming this is the correct type
+        var xrInteractionGroup = controller.GetComponent<XRInteractionGroup>();
+        var calloutGazeController = controller.GetComponent<CalloutGazeController>();
+
+        if (actionBasedControllerManager != null)
+            actionBasedControllerManager.enabled = false;
+
+        if (xrController != null)
+            xrController.enabled = false;
+
+        if (xrInteractionGroup != null)
+            xrInteractionGroup.enabled = false;
+
+        if (calloutGazeController != null)
+            calloutGazeController.enabled = false;
     }
 }
